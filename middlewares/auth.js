b95c449261face4832e91945e0622736c8307c26
Shashidhar Angadi <shashidharangadi14@gmail.com>
2024-02-27 01:26:22 +0530

    Auth Bearer Header usage

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,11 @@
 const { getUser } = require('../service/auth');
 
 const restrictToLoggedinUserOnly = async (req,res,next)=>{
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers['Authorization'];
     
     if(!userUid) return res.redirect('/login');
-    const user = getUser(userUid);
+    const token = userUid.split(' ')[1]; //"Bearer 534452l1kj3"
+    const user = getUser(token);  
     
     if(!user) return res.redirect('./login');
 
@@ -13,9 +14,9 @@ const restrictToLoggedinUserOnly = async (req,res,next)=>{
 }
 
 const checkAuth = async (req,res,next)=>{
-    const userUid = req.cookies?.uid;
-    
-    const user = getUser(userUid);
+    const userUid = req.headers['authorization'];
+    const token = userUid.split(' ')[1];
+    const user = getUser(token);
 
     req.user = user;
     next();
